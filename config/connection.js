4048079ccb1a04b3ac3a8b7b01e018cfa2fcf812
Jennifer Harden <jkfout1@gmail.com>
2019-05-29 18:19:30 -0500

    updated connection for heroku database

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,16 @@
 
 var mysql = require("mysql");
 
-    var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
         host: "localhost",
-        port: 3306,
-        user: process.env.DBconnectionuser,
-        password: process.env.DBconnectionpassword,
+        user: "root",
+        password: "",
         database: "burgers_db"
     });
+};
 
 
 connection.connect(function (err) {
